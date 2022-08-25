bf48d52aa8baa87548dca212bba9d67ad1048f0f
Mike Robbins <mrobbins@alum.mit.edu>
2022-08-25 14:03:10 -0400

    Add last_checked_at, last_collected_at, last_collected_duration methods

--- spec/periodic-gc_spec.cr
+++ spec/periodic-gc_spec.cr
@@ -3,6 +3,9 @@ require "./spec_helper"
 describe PeriodicGC do
   it "starts, stops, and frees memory" do
     PeriodicGC.poll_interval = 1.millisecond
+    PeriodicGC.last_checked_at.should be_nil
+    PeriodicGC.last_collected_at.should be_nil
+    PeriodicGC.last_collected_duration.should be_nil
 
     # Keep pointers around so they aren't freed.
     my_strings = Array(String).new(16)
@@ -11,7 +14,14 @@ describe PeriodicGC do
     GC.stats.bytes_since_gc.should be >= 500000
 
     PeriodicGC.start
+    PeriodicGC.last_checked_at.should be_nil
+    PeriodicGC.last_collected_at.should be_nil
+    PeriodicGC.last_collected_duration.should be_nil
     sleep(15.milliseconds)
+
+    PeriodicGC.last_checked_at.not_nil!.should be_close(Time.monotonic, 5.milliseconds)
+    PeriodicGC.last_collected_at.not_nil!.should be_close(Time.monotonic, 50.milliseconds)
+    PeriodicGC.last_collected_duration.not_nil!.should be < 10.milliseconds
     GC.stats.bytes_since_gc.should be < 16384
 
     my_strings << alloc_string(256000)
@@ -24,8 +34,11 @@ describe PeriodicGC do
     GC.stats.bytes_since_gc.should be < 16384
 
     # Stop PeriodicGC, and make sure allocations just sit around
+    PeriodicGC.last_checked_at.not_nil!.should be_close(Time.monotonic, 5.milliseconds)
     PeriodicGC.stop
     sleep(15.milliseconds)
+    (Time.monotonic - PeriodicGC.last_checked_at.not_nil!).should be < 30.milliseconds
+    (Time.monotonic - PeriodicGC.last_checked_at.not_nil!).should be > 15.milliseconds
     GC.stats.bytes_since_gc.should be < 16384
 
     my_strings << alloc_string(90000)
--- src/periodic-gc.cr
+++ src/periodic-gc.cr
@@ -1,3 +1,10 @@
+# PeriodicGC runs garbage collection periodically to keep memory usage low.
+#
+# To start, just call:
+#
+# ```
+# PeriodicGC.start
+# ```
 class PeriodicGC
   VERSION = "0.1.0"
 
@@ -6,13 +13,32 @@ class PeriodicGC
   @@mu : Mutex = Mutex.new
   @@running : Channel(Nil)? = nil
   @@poll_interval : Time::Span = DEFAULT_POLL_INTERVAL
+  @@last_checked_at : Time::Span? = nil
+  @@last_collected_at : Time::Span? = nil
+  @@last_collected_duration : Time::Span? = nil
 
-  def self.poll_interval=(v : Time::Span)
+  ### PUBLIC METHODS
+
+  # Set the polling interval.
+  #
+  # Polling more frequently will keep memory usage lower, but will burn more CPU time on garbage collection.
+  #
+  # The sweet spot is probably seconds to minutes.
+  def self.poll_interval=(v : Time::Span) : Nil
     @@mu.synchronize do
+      return if @@poll_interval == v
       @@poll_interval = v
+
+      # Restart if changed while running
+      stop_channel = @@running
+      if stop_channel
+        stop_channel.close
+        @@running = spawn_loop
+      end
     end
   end
 
+  # Start a background Fiber that runs garbage collection periodically.
   def self.start : Nil
     @@mu.synchronize do
       return if @@running
@@ -21,6 +47,9 @@ class PeriodicGC
     end
   end
 
+  # Stop periodic garbage collection.
+  #
+  # (You don't need to call this. Just let it die when your program exits.)
   def self.stop : Nil
     @@mu.synchronize do
       stop_channel = @@running
@@ -30,6 +59,48 @@ class PeriodicGC
       end
     end
   end
+
+  # Relative to Time.monotonic, when did PeriodicGC last check whether or not to run garbage collect?
+  #
+  # Returns nil if we've never checked.
+  def self.last_checked_at : Time::Span?
+    @@mu.synchronize do
+      return @@last_checked_at
+    end
+  end
+
+  # Relative to Time.monotonic, when did PeriodicGC last initiate GC.collect?
+  #
+  # Returns nil if we've never collected.
+  def self.last_collected_at : Time::Span?
+    @@mu.synchronize do
+      return @@last_collected_at
+    end
+  end
+
+  # How long did GC.collect take to run?
+  #
+  # Returns nil if we've never collected.
+  def self.last_collected_duration : Time::Span?
+    @@mu.synchronize do
+      return @@last_collected_duration
+    end
+  end
+
+  # Explicitly force collection now.
+  def self.collect : Nil
+    @@mu.synchronize do
+      start_time = Time.monotonic
+      GC.collect
+      end_time = Time.monotonic
+
+      @@last_checked_at = start_time
+      @@last_collected_at = start_time
+      @@last_collected_duration = end_time - start_time
+    end
+  end
+
+  ### INTERNAL METHODS
   
   protected def self.spawn_loop : Channel(Nil)
     stop_channel = Channel(Nil).new
@@ -57,6 +128,14 @@ class PeriodicGC
   end
 
   protected def self.collect_if_needed!
-    GC.collect if GC.stats.bytes_since_gc > 0
+    start_time = Time.monotonic
+    @@last_checked_at = start_time
+    return if GC.stats.bytes_since_gc == 0
+
+    GC.collect
+    end_time = Time.monotonic
+
+    @@last_collected_at = start_time
+    @@last_collected_duration = end_time - start_time
   end
 end

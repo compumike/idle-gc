ea5b36fc943d98790e68259cbf999808eaa93982
Mike Robbins <mrobbins@alum.mit.edu>
2022-08-26 14:01:21 -0400

    IdleGC::Idle.enabled= configuration

--- spec/idle-gc/idle_spec.cr
+++ spec/idle-gc/idle_spec.cr
@@ -22,4 +22,33 @@ describe IdleGC::Idle do
       sleep(100.microseconds)
     end
   end
+
+  it "#enabled= works" do
+    # Should be enabled by default
+    IdleGC::Idle.enabled.should be_true
+    IdleGC::Idle.process_is_idle?.should be_true
+
+    # Start a worker
+    worker_stop_channel = spawn_background_worker(1.millisecond)
+    Fiber.yield
+    IdleGC::Idle.process_is_idle?.should be_false
+
+    # Disable idle detection
+    IdleGC::Idle.enabled = false
+    IdleGC::Idle.enabled.should be_false
+    IdleGC::Idle.process_is_idle?.should be_true
+    IdleGC::Idle.enabled = true
+    IdleGC::Idle.enabled.should be_true
+    IdleGC::Idle.process_is_idle?.should be_false
+    IdleGC::Idle.enabled = false
+    IdleGC::Idle.process_is_idle?.should be_true
+    IdleGC::Idle.enabled = true
+    IdleGC::Idle.process_is_idle?.should be_false
+
+    # Stop the worker
+    worker_stop_channel.close
+    sleep(5.milliseconds)
+    IdleGC::Idle.enabled.should be_true
+    IdleGC::Idle.process_is_idle?.should be_true
+  end
 end
--- src/idle-gc/idle.cr
+++ src/idle-gc/idle.cr
@@ -1,8 +1,10 @@
 class IdleGC
   class Idle
     DEFAULT_IDLE_THRESHOLD = 100.microseconds
-    IDLE_DETECTION_REPEAT = 1
+    DEFAULT_IDLE_DETECTION_REPEAT = 1u8
 
+    @@enabled : Atomic(UInt8) = Atomic(UInt8).new(1u8)
+    @@idle_detection_repeat : Atomic(UInt8) = Atomic(UInt8).new(DEFAULT_IDLE_DETECTION_REPEAT)
     @@idle_threshold : Time::Span = DEFAULT_IDLE_THRESHOLD
 
     # How long does it take for Fiber.yield to return?
@@ -20,13 +22,36 @@ class IdleGC
     #
     # Experimentally, I found Fiber.yield took about ~5us when idle, and ~500us (or more) when busy, but this will depend on your workload.
     def self.idle_threshold=(v : Time::Span) : Nil
-      @@idle_threshold = v
+      IdleGC.mu.synchronize do
+        @@idle_threshold = v
+      end
+    end
+
+    # Idle detection is enabled by default, but it is based on a heuristic which may be inaccurate, so you may wish to disable it by calling `IdleGC::Idle.enabled = false`.
+    #
+    # If disabled, then `#process_is_idle?` will always return true.
+    #
+    # If your workload is not latency-sensitive, then running GC periodicially regardless of whether the system is idle may be optimal.
+    def self.enabled=(v : Bool) : Nil
+      @@enabled.set(v ? 1u8 : 0u8)
+    end
+
+    def self.enabled : Bool
+      @@enabled.get == 1u8
+    end
+
+    # To reduce idle false-positives, we can check multiple times in succession. Default is 1.
+    def self.idle_detection_repeat=(v : UInt8) : Nil
+      raise ArgumentError.new("Must be at least 1") if v == 0
     end
 
     # Return true if the process is idle, as determined by `#fiber_yield_time` compared to `#idle_threshold=`.
     def self.process_is_idle? : Bool
+      return true unless enabled
+
       # Check multiple times to reduce false positive rate
-      IDLE_DETECTION_REPEAT.times.all? { fiber_yield_time < @@idle_threshold }
+      idle_detection_repeat = @@idle_detection_repeat.get
+      idle_detection_repeat.times.all? { fiber_yield_time < @@idle_threshold }
     end
   end
 end

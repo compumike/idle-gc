d195f3050eb9ae4a27cf8d000f7bb4b22aa2cb78
Mike Robbins <mrobbins@alum.mit.edu>
2022-08-29 21:07:50 -0400

    We have a benchmark

--- bench/idle-gc-bench.cr
+++ bench/idle-gc-bench.cr
@@ -4,7 +4,10 @@ require "option_parser"
 require "../src/idle-gc"
 
 class IdleGCBench
-  ALLOC_STRING_SIZE = 1000000u64
+  ALLOC_STRING_SIZE = 2000000u64
+  REQUEST_BATCHES = 50
+  REQUEST_COUNT = 5
+  REQUEST_BATCH_DELAY = 200.milliseconds
 
   @start = false
   getter sync_every : Bool = false
@@ -80,7 +83,21 @@ class IdleGCBench
       IdleGC::Request.request if @bench.request_limit > 0
     end
 
+    def wait_for_requested_at :  Nil
+      now = Time.monotonic
+      loop do
+        break if now >= @requested_at
+
+        sleep_for = @requested_at - now
+        sleep(sleep_for)
+
+        now = Time.monotonic
+      end
+    end
+
     def process
+      wait_for_requested_at
+
       @processing_started_at = Time.monotonic
       @result = do_request
       @processing_ended_at = Time.monotonic
@@ -99,28 +116,67 @@ class IdleGCBench
     end
   end
 
+  def proc_stat : String
+    File.read("/proc/#{Process.pid}/stat")
+  end
 
-  def start
-    5.times { IdleGC.collect }
+  def rss_pages : UInt64
+    proc_stat.split(" ")[23].to_u64
+  end
 
-    start_time = Time.monotonic
-    1000.times do
-      r = Request.new(self)
+  def rss_bytes : UInt64
+    rss_pages * 4096
+  end
+
+  def spawn_request(completion_channel : Channel(Nil), start_at : Time::Span? = nil) : Nil
+    spawn do
+      r = Request.new(self, start_at)
       @requests << r
       r.process
       Fiber.yield
       @latencies << r.latency.not_nil!
+      completion_channel.send(nil)
+    end
+  end
+
+  def start
+    5.times { IdleGC.collect }
+
+    ### BEGIN REQUESTS
+
+    start_time = Time.monotonic
+    completion_channel = Channel(Nil).new(REQUEST_BATCHES * REQUEST_COUNT)
+    batch_time = start_time
+    REQUEST_BATCHES.times do
+      REQUEST_COUNT.times do
+        spawn_request(completion_channel, batch_time)
+      end
+      batch_time += REQUEST_BATCH_DELAY
+    end
+    (REQUEST_BATCHES * REQUEST_COUNT).times do
+      completion_channel.receive
     end
     end_time = Time.monotonic
 
+    ### END REQUESTS
+
+    gc_stats_before_compact = GC.stats
+    5.times { IdleGC.collect }
     gc_stats = GC.stats
+
     @latencies.sort!
     runtime_s = (end_time - start_time).total_seconds
     heap_size_mib = gc_stats.heap_size.to_f64 / (1024.0*1024.0)
+    rss_mib = rss_bytes.to_f64 / (1024.0*1024.0)
+    used_mib = (gc_stats.heap_size - gc_stats.free_bytes).to_f64 / (1024.0*1024.0)
 
-    puts "Runtime:     #{runtime_s.format(decimal_places: 2)} s"
-    puts "Heap size:   #{heap_size_mib.format(decimal_places: 2)} MiB"
-    puts "Max latency: #{@latencies.last} s"
+    puts "RSS:            #{rss_mib.format(decimal_places: 2)} MiB"
+    puts "Runtime:        #{runtime_s.format(decimal_places: 2)} s"
+    puts "Heap size:      #{heap_size_mib.format(decimal_places: 2)} MiB"
+    puts "Used size:      #{used_mib.format(decimal_places: 2)} MiB"
+    puts "Max latency:    #{@latencies.last} s"
+    puts "GC stats (pre): #{gc_stats}"
+    puts "GC stats:       #{gc_stats}"
   end
 
   def initialize
@@ -131,6 +187,7 @@ class IdleGCBench
       parser.on("--background-every", "Background GC after every request") { @bg_every = true }
       parser.on("--request-limit=N", "Set IdleGC::Request.request_limit and fire a request after every request") { |v| @request_limit = v.to_u64 }
       parser.on("--start", "Start IdleGC::Timer") { @start = true }
+      parser.on("--poll=N", "IdleGC::Timer.poll_interval=") { |v| IdleGC::Timer.poll_interval = v.to_f64.seconds }
     end
 
     IdleGC.start if @start
